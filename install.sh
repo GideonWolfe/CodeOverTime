866d1475a90e40ab6c091e45e428c538241d1c83
Gideon Wolfe <wolfeg2@wwu.edu>
2019-07-12 08:13:02 +0000

    install and update scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run as root, this will add the script to your path
+chmod +x *.sh
+cp ./CodeOverTime.sh /usr/bin/codeovertime
---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run as root, this will add the script to your path
+git pull origin master
+cp ./CodeOverTime.sh /usr/bin/codeovertime
