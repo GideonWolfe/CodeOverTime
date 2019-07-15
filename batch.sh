5cd834132c63b209e1d34de6faef16ddf2eecb3d
Gideon Wolfe <wolfeg2@wwu.edu>
2019-07-15 18:53:09 +0000

    added a batch hadler script

---
+++ batch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script is meant to act as a handler for the main program.
+# Use this if you wanted to preform a batch line count on multiple repositories
+# RUN THIS IN A CLEAN DIRECTORY, unless you want every repo to be newly cloned.
+
+
+declare -a REPOSITORIES
+
+# Fill in your repositories here
+REPOSITORIES=(
+''
+)
+
+# Clone all the repositories
+#for i in "${REPOSITORIES[@]}"
+#do
+#	git clone $i
+#done
+
+# For every directory calculate the lines
+# and then output a JSON to the root directory
+for dir in */
+do
+	cd "$dir"
+	mv ${dir%/}.json ..
+	codeovertime -j
+	cd ..
+done
